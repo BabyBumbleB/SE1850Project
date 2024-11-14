678737e6566516758223e6da3ce455ac510a12c6
Trickybone <137672182+Trickybone@users.noreply.github.com>
2024-11-14 12:04:15 -0600

    Update Snake.c

--- Snake.c
+++ Snake.c
@@ -3,6 +3,7 @@
 
 #define width 20 
 #define height 20
+#define grid[width][height]
 
 int x, y, fruit, gameOver, key, score; //x and y are for the starting coords of the snake
 int tailLen = 3; //I always have seen the tail initialized to three so why not
@@ -12,5 +13,11 @@ void setUp(){
 }
 
 int main(int argc, char* argv[]){
-    
+    for(int i = 0; i < width; i++)
+        {
+           for (j = 0; j < height; j++)
+               {
+                    printf("%d\n". grid[i][j]);
+               }
+        }
 }
