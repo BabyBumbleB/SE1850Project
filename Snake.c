74237eacb95b6fb137613d5b405b7621184538f2
unknown <hadenn3@gmail.com>
2024-11-15 03:56:30 +0000

    90% Fixed grid logic. Renamed functions to draw and setup. Added input and logic functions

--- Snake.c
+++ Snake.c
@@ -2,56 +2,64 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-#define width 20 
+#define width 20
 #define height 20
 
+int x = 0, y = 0, fruit; 
+int gameOver;
 
-int x, y, fruit, gameOver, key, score; //x and y are for the starting coords of the snake
-int tailLen = 3; //I always have seen the tail initialized to three in the games, so why not
-
-void plotMap(void);
-void setUp(void);
+int snakeLen;
 
 
-void setUp(){
+
+
+
+
+void setup(){ // Intailzize
     //find starting point
+    
+    snakeLen = 1;
     x = width / 2; 
     y = height / 2;
+    
     gameOver = 0; //initialize gameOver to false, which will later be true
-    plotMap();
+    
 }
 
-void plotMap(){ // README: Fix Map using N Curses? Use Draw function and change the fixed variables to width and height.
-    
+void draw(){ // Runs per frame README: Fix Map using N Curses? Use Draw function and change the fixed variables to width and height.
     char grid[width][height];
-    
-    for (int i = 0; i < 20; i++) //Row
+    for (int i = 0; i < width; i++) // Row
         {
-          for (int j = 0; j < 20; j++) //Col
-              {
-                    grid[i][j] = '.';
-              }
-        }
-    
-    //Intalize Top Border
-    for (int i = 0; i < 1; i++) //Row
-        {
-           for (int j = 0; j < 20; j++) //Col
-               {
+        
+          for (int j = 0; j < height; j++) // Col
+            {
+                grid[i][j] = '.'; // No Idea why this works
+                
+                if(i == 0 || i == width - 1){ // Top Border or Bottom Border
                     grid[i][j] = '-';
-               }
+                }
+
+                if(i == i && j == 0 || j == width - 1){ // Left Border or right border
+                    grid[i][j] = '|';
+                }
+
+                if(i == x && j == y){ // Spawn point middle
+                    grid[x][y] = 'O';
+                }
+
+
+
+
+                
+            }
         }
+
+   
+        
         
     
         
-    //Intalize Bottom Border
-    for (int i = 19; i < 20; i++)
-        {
-           for (int j = 0; j < 20; j++)
-               {
-                    grid[i][j] = '-';
-               }
-        }
+    
         
     //Print map
     for (int i = 0; i < 20; i++)
@@ -64,6 +72,33 @@ void plotMap(){ // README: Fix Map using N Curses? Use Draw function and change 
         }
 }
 
+void input()
+{
+    char getKey = scanf(" %c ");
+
+    switch(getKey)
+    {
+        case 'w':   
+            //w
+            break;
+        case 'a':
+            //a
+            break;
+        case 's':
+            //s
+            break;
+        case 'd':
+            //d
+            break;
+    }
+}
+
+void logic()
+{
+    
+}
+
 int main(int argc, char* argv[]){
-    setUp();
+    setup();
+    draw();
 }
