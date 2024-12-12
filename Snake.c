e59a06308351472600f38c8ffd04221645a00f54
Trickybone <137672182+Trickybone@users.noreply.github.com>
2024-12-11 20:13:28 -0600

    Update Snake.c
    Snake moves automatically and apples spawns

--- Snake.c
+++ Snake.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <ncurses.h> //remember to do -lncurses when compiling or running, i forgor which one :skull:
 #include <curses.h>
-
-//#define getmaxyx(win,y,x)	(y = getmaxy(win), x = getmaxx(win))
+#include <unistd.h>
 
 int cordX, cordY, fruitx, fruity, gameOver;
 int snakeTailX[100], snakeTailY[100];
@@ -11,15 +10,14 @@ int snakeLen, snakeLenx, snakeLeny;
 int width = 20;
 int height = 20;
 int score = 0;
-char keyPressed;
-char storeKey;
+int keyPressed;
+int storeKey;
 
 void setup()
 {     
     initscr();
     noecho(); 
-    
-    //getmaxyx(stdscr, height, width);
+    keypad(stdscr, TRUE);
 
     snakeLen = 3;
     cordX = width / 2; //starting coords of snake head
@@ -41,22 +39,21 @@ void draw(){
                 grid[i][j] = '.'; 
                 
                 if(i == 0 || i == height - 1){ 
-                    grid[i][j] = '-'; //top of grid
+                    grid[i][j] = '-'; // Top and Bottom Grid
                 }else if(j == 0 || j == width - 1){ 
-                    grid[i][j] = '|'; //side walls of grid
+                    grid[i][j] = '|'; // Side walls of Grid
                 }else if(i == cordY && j == cordX){ 
-                    grid[i][j] = 'O'; //snake head
+                    grid[i][j] = 'O'; // Snake head
+                }else if(i == fruity && j == fruitx){ 
+                    grid[i][j] = '*';
                 }else{
-                for(int k = 0; k < snakeLen; k++){
-                    if((snakeTailX[k] == j) && (snakeTailY[k] == i)){
-                        grid[i][j] = 'o'; //snake body
-                        
-                    }
+                    for(int k = 0; k < snakeLen; k++){
+                        if((snakeTailX[k] == j) && (snakeTailY[k] == i)){
+                            grid[i][j] = 'o'; // Snake body
+                            
+                        }
 
-                }
-
-
-                    
+                    }   
                 }
             }
         }
@@ -68,34 +65,34 @@ void draw(){
                     mvaddch(i, j, grid[i][j]);
                }
         }
+    mvprintw(height, 0, "Score : %d", score);
+    mvprintw(height + 1, 0, "Created by Haden and Ben");
 }
 
 void input()
 {
-    keyPressed = getch();
+    keyPressed = getch();    
     switch(keyPressed) //getting input from user and storing it
     {
-        case 'w':   
-            storeKey = 'w';
-            break;
-        case 'a':
-            storeKey = 'a';
-            break;
-        case 's':
-            storeKey = 's';
-            break;
-        case 'd':
-            storeKey = 'd';
-            break;
-        default: 
-            break;
+        case 'w': if(storeKey != 3) { storeKey = 1; } break;
+        case 'a': if(storeKey != 4) { storeKey = 2; } break;
+        case 's': if(storeKey != 1) { storeKey = 3; } break;
+        case 'd': if(storeKey != 2) { storeKey = 4; } break;
+        default: break;
+    }
+    switch(keyPressed)
+    {
+        case KEY_UP: if(storeKey != 3) { storeKey = 1; } break;
+        case KEY_LEFT: if(storeKey != 4) { storeKey = 2; } break;
+        case KEY_DOWN: if(storeKey != 1) { storeKey = 3; } break;
+        case KEY_RIGHT: if(storeKey != 2) { storeKey = 4; } break;
+        default: break;
     }
 }
 
 void logic()
 {
-
-        int prevX = snakeTailX[0];
+    int prevX = snakeTailX[0];
     int prevY = snakeTailY[0];
     int prev2X, prev2Y;
     snakeTailX[0] = cordX;
@@ -109,40 +106,33 @@ void logic()
         prevX = prev2X;
         prevY = prev2Y;
     }
+    
+    
     switch(storeKey) //actually making the snake move based off of user input
     {
-        case 'w':   
-            cordY--;
-            break;
-        case 'a':
-            cordX--;
-            break;
-        case 's':
-            cordY++;
-            break;
-        case 'd':
-            cordX++;
-            break;
-        default: 
-            break; 
-    }
-    //refresh()
+        case 1: cordY--; break;
+        case 2: cordX--; break;
+        case 3: cordY++; break;
+        case 4: cordX++; break;
+        default: break;
+    } 
+    
 
     if(cordX < 0 || cordX >= width || cordY < 0 || cordY >= height){
         gameOver = 1; //if snake leaves the parameters, gameover. 
     }
     
-for (int i = 0; i < snakeLen; i++) {
+    for (int i = 0; i < snakeLen; i++) {
         if ((snakeTailX[i] == cordX) && (snakeTailY[i] == cordY))
             gameOver = 1; //if the snake collides with its body, gameover
     }
 
-if((cordX == fruitx) && (cordY == fruity)){
-fruitx = rand() % (width - 1) + 1; //randomizing the location spawns of the fruit
-fruity = rand() % (height - 1) + 1;
-score += 100; //just random score incrementation based on when it touched a fruit
-snakeLen++;
-}
+    if((cordX == fruitx) && (cordY == fruity)){
+        fruitx = rand() % (width - 1) + 1; //randomizing the location spawns of the fruit
+        fruity = rand() % (height - 1) + 1;
+        score += 100; //just random score incrementation based on when it touched a fruit
+        snakeLen++;
+    }
     
 }
 
@@ -150,13 +140,15 @@ snakeLen++;
     //subtracting them
 
 int main(int argc, char* argv[]){
-    setup();
     
+    setup();
     while(!gameOver){ //leave these in this order
         draw();
         input(); 
         logic();
+        halfdelay(1);
     }
-endwin(); //this fixes a bug that didnt let me type in the terminal once the game ended
+    
+    endwin(); //this fixes a bug that didnt let me type in the terminal once the game ended
     return 0;
 }
